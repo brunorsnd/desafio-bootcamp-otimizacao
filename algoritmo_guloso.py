6f61eb9c8febb68282fead0e82b8550cd915a55c
Bruno Resende <brunorsnd7@hotmail.com>
2023-02-09 21:48:04 -0300

    Variáveis para teste retiradas do código

--- algoritmo_guloso.py
+++ algoritmo_guloso.py
@@ -1,17 +1,3 @@
-my_capital = 1000000
-descriptions = ['Ampliação da capacidade do armazém ZDP em 5%', 'Ampliação da capacidade do armazém MGL em 7%',
-                'Compra de empilhadeira', 'Projeto de P&D I', 'Projeto de P&D II', 'Aquisição de novos equipamentos',
-                'Capacitação de funcionários', 'Ampliação da estrutura de carga rodoviária']
-costs = [470000, 400000, 170000, 270000, 340000, 230000, 50000, 440000]
-returns = [410000, 330000, 140000, 250000, 320000, 320000, 90000, 190000]
-restrictions = [(1, 'nao, se', 4), (3, 'se', 1)]  # 1 + 4 <= 1    3 >= 1
-
-capital2 = 150
-custos2 = [50, 50, 50]
-retornos2 = [60, 50, 40]
-restricoes2 = [(1, 'nao, se', 2)]
-
-
 def algoritmo_guloso(capital, custos, retornos, restricoes=None):
     razoes = []
 
@@ -25,7 +11,7 @@ def algoritmo_guloso(capital, custos, retornos, restricoes=None):
     for indice, razao in razoes:
         if restricoes is not None:
             for a, tipo, b in restricoes:
-                if a == indice and tipo == 'nao, se' and b in selecionados:
+                if a == indice and tipo == 'nao, se' and b in selecionados:  #
                     descartados.append(a)
                 if b == indice and tipo == 'nao, se' and a in selecionados:
                     if b not in descartados:
